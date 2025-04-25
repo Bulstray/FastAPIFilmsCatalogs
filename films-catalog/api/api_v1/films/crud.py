415920642c26f06e6cbdaf3d6249524812e9a88a
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-04-25 14:16:51 +0300

    Create crud.py

---
+++ films-catalog/api/api_v1/films/crud.py
@@ -0,0 +1,19 @@
+from schemas.film import Film
+
+FILMS = [
+    Film(
+        movie_id=1,
+        name="Остров проклятых",
+        description="Фильм про психбольницу",
+    ),
+    Film(
+        movie_id=2,
+        name="Джентельмены",
+        description="Фильм про мафию",
+    ),
+    Film(
+        movie_id=3,
+        name="Область тьмы",
+        description="Фильм про работу мозга",
+    ),
+]
