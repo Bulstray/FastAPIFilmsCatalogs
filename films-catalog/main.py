83955f68c97814e263d8861149fe6be2f48530d3
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-04-25 09:29:33 +0300

    Update main.py: add a view for the root view

--- films-catalog/main.py
+++ films-catalog/main.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
 
 app = FastAPI(
     title='Films Catalog'
 )
+
+
+@app.get('/')
+def read_docs(request: Request):
+    docs_url = request.url.replace(
+        path='/docs',
+    )
+
+    return {
+        'docs': str(docs_url)
+    }
