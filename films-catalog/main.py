f9842f5be66a9eb45658bc76454c5f142d3bff07
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-04-25 14:11:55 +0300

    Refactor main.py

--- films-catalog/main.py
+++ films-catalog/main.py
@@ -1,11 +1,11 @@
-from typing import Annotated
-
 import uvicorn
-from fastapi import FastAPI, Request, Depends, HTTPException, status
+from fastapi import FastAPI, Request
 
-from schemas.film import Film
+from api import router as api_router
 
 app = FastAPI(title="Films Catalog")
+
+app.include_router(api_router)
 
 
 @app.get("/")
@@ -15,58 +15,6 @@ def read_docs(request: Request):
     )
 
     return {"docs": str(docs_url)}
-
-
-FILMS = [
-    Film(
-        movie_id=1,
-        name="Остров проклятых",
-        description="Фильм про психбольницу",
-    ),
-    Film(
-        movie_id=2,
-        name="Джентельмены",
-        description="Фильм про мафию",
-    ),
-    Film(
-        movie_id=3,
-        name="Область тьмы",
-        description="Фильм про работу мозга",
-    ),
-]
-
-
-@app.get(
-    "/films/",
-    response_model=list[Film],
-)
-def read_film_list():
-    return FILMS
-
-
-def prefetch_film_by_id(movie_id: int) -> Film:
-    film: Film | None = next(
-        (film for film in FILMS if film.movie_id == movie_id),
-        None,
-    )
-
-    if film:
-        return film
-
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"movie by {movie_id!r} not found",
-    )
-
-
-@app.get("/film/{movie_id}")
-def get_film_by_id(
-    film: Annotated[
-        Film,
-        Depends(prefetch_film_by_id),
-    ],
-):
-    return film
 
 
 if __name__ == "__main__":
