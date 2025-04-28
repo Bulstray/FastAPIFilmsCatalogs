dc670e7b0e5c51bbcb1c07857bd35107248233d5
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-04-28 14:28:24 +0300

    Update Crud: Save date memory

--- films-catalog/api/api_v1/films/crud.py
+++ films-catalog/api/api_v1/films/crud.py
@@ -1,19 +1,43 @@
-from schemas.film import Film
+from pydantic import BaseModel
+
+from schemas.film import Film, FilmCreate
 
 FILMS = [
-    Film(
+    FilmCreate(
         slug="abc",
         name="Остров проклятых",
         description="Фильм про психбольницу",
     ),
-    Film(
+    FilmCreate(
         slug="foo",
         name="Джентельмены",
         description="Фильм про мафию",
     ),
-    Film(
+    FilmCreate(
         slug="bar",
         name="Область тьмы",
         description="Фильм про работу мозга",
     ),
 ]
+
+
+class FilmsStorage(BaseModel):
+    slug_to_film: dict[str, Film] = {}
+
+    def get(self) -> list[Film]:
+        return [self.slug_to_film[film] for film in self.slug_to_film]
+
+    def get_by_slug(self, slug):
+        return self.slug_to_film.get(slug)
+
+    def create(self, film: FilmCreate) -> Film:
+        film = Film(**film.model_dump())
+        self.slug_to_film[film.slug] = film
+        return film
+
+
+storage = FilmsStorage()
+
+
+for film in FILMS:
+    storage.create(film=film)
