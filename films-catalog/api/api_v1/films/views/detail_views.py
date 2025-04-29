2cca57a381963d3dbc9096df8370c4958890333a
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-04-29 09:59:21 +0300

    add views update

--- films-catalog/api/api_v1/films/views/detail_views.py
+++ films-catalog/api/api_v1/films/views/detail_views.py
@@ -4,36 +4,40 @@ from fastapi import Depends, APIRouter, status
 
 from api.api_v1.films.crud import storage
 from api.api_v1.films.dependencies import prefetch_film_by_id
-from schemas.film import Film
+from schemas.film import Film, FilmUpdate
 
 
 router = APIRouter()
+
+
+MovieBySlug = Annotated[Film, Depends(prefetch_film_by_id)]
 
 
 @router.get(
     "/film/{slug}",
     response_model=Film,
 )
-def get_film_by_id(
-    film: Annotated[
-        Film,
-        Depends(prefetch_film_by_id),
-    ],
-):
+def get_film_by_id(film: MovieBySlug):
     return film
+
+
+@router.put(
+    "/{slug}/",
+    response_model=Film,
+)
+def update_film_details(
+    movie: MovieBySlug,
+    movie_in: FilmUpdate,
+):
+    return storage.update(
+        movie=movie,
+        movie_in=movie_in,
+    )
 
 
 @router.delete(
     "/slug/",
     status_code=status.HTTP_204_NO_CONTENT,
-    responses={
-        status.HTTP_404_NOT_FOUND: {
-            "description": "Film not found",
-            "content": {
-                "application/json": {"detail": "Film 'slug' not found"},
-            },
-        },
-    },
 )
 def delete_movie(
     movie: Annotated[
