3dc137eb2e44516bc47a235d75172195a98412f4
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-04-29 09:16:20 +0300

    Create Router

---
+++ films-catalog/api/api_v1/films/views/__init__.py
@@ -0,0 +1,12 @@
+__all__ = ("router",)
+
+from fastapi import APIRouter
+
+from .list_views import router as list_router
+from .detail_views import router as detail_router
+
+
+router = APIRouter(prefix="/films", tags=["Films"])
+
+router.include_router(list_router)
+router.include_router(detail_router)
