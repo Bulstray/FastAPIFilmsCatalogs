4af700d2764165dba100a44b34bb022b7d8a2b45
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-04-29 14:17:58 +0300

    Rename name and Annotation

--- films-catalog/api/api_v1/films/views/list_views.py
+++ films-catalog/api/api_v1/films/views/list_views.py
@@ -2,15 +2,14 @@ from fastapi import APIRouter, status
 
 from api.api_v1.films.crud import storage
 
-from schemas.film import Film, FilmCreate
-
+from schemas.movie import Movie, MovieCreate, MovieRead
 
 router = APIRouter()
 
 
 @router.get(
     "/",
-    response_model=list[Film],
+    response_model=list[MovieRead],
 )
 def read_film_list():
     return storage.get()
@@ -18,10 +17,8 @@ def read_film_list():
 
 @router.post(
     "/",
-    response_model=Film,
+    response_model=Movie,
     status_code=status.HTTP_201_CREATED,
 )
-def add_film(create_film: FilmCreate):
-    return Film(
-        **create_film.model_dump(),
-    )
+def add_film(create_film: MovieCreate):
+    return storage.create(film=create_film)
