1c784b4aed01abb7471265928c4ad11864d00b77
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-04-30 15:01:53 +0300

    add func dependencies

--- films-catalog/api/api_v1/films/dependencies.py
+++ films-catalog/api/api_v1/films/dependencies.py
@@ -1,8 +1,12 @@
-from fastapi import HTTPException
+import logging
+
+from fastapi import HTTPException, BackgroundTasks
 from starlette import status
 
 from .crud import storage
 from schemas.movie import Movie
+
+log = logging.getLogger(__name__)
 
 
 def prefetch_film_by_id(slug) -> Movie:
@@ -15,3 +19,9 @@ def prefetch_film_by_id(slug) -> Movie:
         status_code=status.HTTP_404_NOT_FOUND,
         detail=f"movie by {slug!r} not found",
     )
+
+
+def save_storage_state(background_tasks: BackgroundTasks):
+    yield
+    log.info("Add background task to save storage")
+    background_tasks.add_task(storage.save_state)
