b198319b91e733a845d1a7dc2bc12ef50c239d14
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-04-30 15:01:06 +0300

    add dependencies in router

--- films-catalog/api/api_v1/films/views/__init__.py
+++ films-catalog/api/api_v1/films/views/__init__.py
@@ -1,12 +1,17 @@
 __all__ = ("router",)
 
 from fastapi import APIRouter
+from fastapi.params import Depends
 
 from .list_views import router as list_router
 from .detail_views import router as detail_router
+from ..dependencies import save_storage_state
 
-
-router = APIRouter(prefix="/films", tags=["Films"])
+router = APIRouter(
+    prefix="/films",
+    tags=["Films"],
+    dependencies=[Depends(save_storage_state)],
+)
 
 router.include_router(list_router)
 router.include_router(detail_router)
