ffe00874a37b6c0d5fbc92137b22a4da3ed77fcc
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-05-05 09:56:14 +0300

    Add Depends check token

--- films-catalog/api/api_v1/films/views/__init__.py
+++ films-catalog/api/api_v1/films/views/__init__.py
@@ -5,12 +5,15 @@ from fastapi.params import Depends
 
 from .list_views import router as list_router
 from .detail_views import router as detail_router
-from ..dependencies import save_storage_state
+from ..dependencies import save_storage_state, api_token_required
 
 router = APIRouter(
     prefix="/films",
     tags=["Films"],
-    dependencies=[Depends(save_storage_state)],
+    dependencies=[
+        Depends(save_storage_state),
+        Depends(api_token_required),
+    ],
 )
 
 router.include_router(list_router)
