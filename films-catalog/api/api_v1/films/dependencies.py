1d7659cfde3f53c9f23285f2eb5b41ab273cea5b
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-05-06 11:15:19 +0300

    add auth api token or auth

--- films-catalog/api/api_v1/films/dependencies.py
+++ films-catalog/api/api_v1/films/dependencies.py
@@ -90,11 +90,15 @@ user_basic_auth = HTTPBasic(
 
 
 def user_basic_auth_required(
+    request: Request,
     credentials: Annotated[
         HTTPBasicCredentials | None,
         Depends(user_basic_auth),
     ],
 ):
+
+    if request.method not in UNSAFE_METHODS:
+        return
 
     if (
         credentials
@@ -109,4 +113,56 @@ def user_basic_auth_required(
         headers={
             "WWW-Authenticate": "Basic",
         },
+    )
+
+
+def validate_api_token(api_token: HTTPAuthorizationCredentials):
+    if api_token.credentials not in API_TOKENS:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid API token"
+        )
+
+
+def validate_basic_auth(credentials: HTTPBasicCredentials | None):
+    if (
+        credentials
+        and credentials.username in USERS_DB
+        and USERS_DB[credentials.username] == credentials.password
+    ):
+        return
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid username or password",
+        headers={"WWW-Authenticate": "Basic"},
+    )
+
+
+def api_token_or_auth_required_for_unsafe_methods(
+    request: Request,
+    api_token: Annotated[
+        HTTPAuthorizationCredentials | None,
+        Depends(static_api_token),
+    ] = None,
+    credentials: Annotated[
+        HTTPBasicCredentials | None,
+        Depends(user_basic_auth),
+    ] = None,
+):
+    if request.method not in UNSAFE_METHODS:
+        return
+
+    if credentials:
+        return validate_basic_auth(
+            credentials=credentials,
+        )
+
+    if api_token:
+        return validate_api_token(
+            api_token=api_token,
+        )
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="API token or basic auth required",
     )
