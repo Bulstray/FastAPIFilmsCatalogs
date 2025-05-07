edd9c7b53f6336e2baa49cea27fb77e6410a6ea3
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-05-07 12:56:24 +0300

    Add Redis DB users

--- films-catalog/core/config.py
+++ films-catalog/core/config.py
@@ -10,23 +10,11 @@ LOG_FORMAT: str = (
     "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
 )
 
-API_TOKENS = frozenset(
-    {
-        "nWkE1Wd-_TUBP493xjRXng",
-        "Gw28yx5gjLQHa0I4REEDGQ",
-    },
-)
-
-
-USERS_DB: dict[str, str] = {
-    "sam": "password",
-    "bob": "qwerty",
-}
-
 
 REDIS_HOST = "localhost"
 REDIS_PORT = 6379
 REDIS_DB = 0
 REDIS_DB_TOKENS = 1
-
 REDIS_TOKES_SET_NAME = "tokens"
+
+REDIS_DB_USERS = 2
