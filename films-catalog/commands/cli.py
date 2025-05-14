37bb9d87748154d8066e0ecdbe56ac9547afc99c
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-05-14 11:50:24 +0300

    Add token managements

--- films-catalog/commands/cli.py
+++ films-catalog/commands/cli.py
@@ -1,6 +1,7 @@
 from typer import Typer
 
 from .hello import app as hello_app
+from .tokens import app as token_app
 
 app = Typer(
     no_args_is_help=True,
@@ -18,3 +19,4 @@ def callback():
 app.add_typer(
     hello_app,
 )
+app.add_typer(token_app)
