c266e72bf3aec1d5bab740f8633ec68be4fe66ba
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-05-14 10:35:56 +0300

    callback typer

---
+++ films-catalog/commands/cli.py
@@ -0,0 +1,20 @@
+from typer import Typer
+
+from .hello import app as hello_app
+
+app = Typer(
+    no_args_is_help=True,
+    rich_markup_mode="rich",
+)
+
+
+@app.callback()
+def callback():
+    """
+    Some CLI management commands
+    """
+
+
+app.add_typer(
+    hello_app,
+)
