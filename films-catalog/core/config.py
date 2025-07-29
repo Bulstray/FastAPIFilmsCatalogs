6fc6cded668baeabf43fd26e3b89ec698bf21ff8
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-07-29 13:38:04 +0300

    Add extended setting

--- films-catalog/core/config.py
+++ films-catalog/core/config.py
@@ -1,7 +1,12 @@
 import logging
+from os import getenv
+from pathlib import Path
+from typing import Literal, Self
 
-from pydantic import BaseModel
-from pydantic_settings import BaseSettings
+from pydantic import BaseModel, model_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 LOG_FORMAT: str = (
     "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
@@ -9,14 +14,24 @@ LOG_FORMAT: str = (
 
 
 class LoggingConfig(BaseModel):
-    log_level: int = logging.INFO
+    log_level_name: Literal[
+        "DEBUG",
+        "INFO",
+        "WARNING",
+        "ERROR",
+        "CRITICAL",
+    ] = "INFO"
     log_format: str = LOG_FORMAT
     date_format: str = "%Y-%m-%d %H:%M:%S"
+
+    @property
+    def log_level(self) -> int:
+        return logging.getLevelNamesMapping()[self.log_level_name]
 
 
 class RedisConnectionConfig(BaseModel):
     host: str = "localhost"
-    port: int = 6379
+    port: int = 6380 if getenv("TESTING") else 6379
 
 
 class RedisDatabaseConfig(BaseModel):
@@ -24,6 +39,14 @@ class RedisDatabaseConfig(BaseModel):
     tokens: int = 1
     users: int = 2
     movies: int = 3
+
+    @model_validator(mode="after")
+    def validate_dbs_numbers_unique(self) -> Self:
+        db_values = list(self.model_dump().values())
+
+        if len(set(db_values)) != len(db_values):
+            raise ValueError("Database numbers should be unique")
+        return self
 
 
 class RedisCollectionConfig(BaseModel):
@@ -38,9 +61,25 @@ class RedisConfig(BaseModel):
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        case_sensitive=False,
+        env_file=(
+            BASE_DIR / ".env.template",
+            BASE_DIR / ".env",
+        ),
+        env_prefix="FILMS_CATALOG__",
+        env_nested_delimiter="__",
+        yaml_file=(
+            BASE_DIR / "config.default.yaml",
+            BASE_DIR / "config.local.yaml",
+        ),
+        yaml_config_section="movies",
+    )
     logging: LoggingConfig = LoggingConfig()
     redis: RedisConfig = RedisConfig()
 
 
 # noinspection PyArgumentList
 settings = Settings()
+
+print(settings.redis.connection.port)
