2eca3915ada98b4d575c550b44bccb57a34d2580
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-07-29 13:38:30 +0300

    Update conftest.py

--- films-catalog/tests/conftest.py
+++ films-catalog/tests/conftest.py
@@ -1,6 +1,43 @@
+import random
+import string
 from os import getenv
+from typing import Generator
 
 import pytest
 
+from api.api_v1.films.crud import storage
+from schemas.movie import Movie
+
 if getenv("TESTING") != "1":
     pytest.exit("Environment is not ready for tests")
+
+
+def build_movie_create(slug: str) -> Movie:
+    return Movie(
+        slug=slug,
+        description="Some description",
+        name="Some name",
+    )
+
+
+def build_movie_random_slug() -> Movie:
+    return build_movie_create(
+        slug="".join(
+            random.choices(
+                string.ascii_letters,
+                k=8,
+            ),
+        ),
+    )
+
+
+def create_movie(slug: str) -> Movie:
+    movie = build_movie_create(slug)
+    return storage.create(movie)
+
+
+@pytest.fixture()
+def movie() -> Generator[Movie]:
+    movie = build_movie_random_slug()
+    yield movie
+    storage.delete(movie)
