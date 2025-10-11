93b646f073b72b18eb1de86d8d128b6f20996777
Ramil Khakimov <bulstray@yandex.ru>
2025-10-11 13:02:58 +0300

    refactor list_views

---
+++ films-catalog/rest/movies/list_views.py
@@ -0,0 +1,26 @@
+from typing import Any
+
+from fastapi import APIRouter
+from fastapi.requests import Request
+from fastapi.responses import HTMLResponse
+
+from dependencies.movies import GetMoviesStorage
+from templating import templates
+
+router = APIRouter()
+
+
+@router.get("/", name="movies:list", response_class=HTMLResponse)
+def list_view(
+    request: Request,
+    storage: GetMoviesStorage,
+) -> HTMLResponse:
+    context: dict[str, Any] = {}
+    movies: list = storage.get()
+    context.update(movies=movies)
+
+    return templates.TemplateResponse(
+        request=request,
+        name="movies/list.html",
+        context=context,
+    )
