aaf2adeeb54c5e25195c670a4c7f9cdb0eff4413
Ramil Khakimov <bulstray@yandex.ru>
2025-10-11 13:03:53 +0300

    refactor file main_views

---
+++ films-catalog/rest/main_views.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter
+from fastapi.requests import Request
+from fastapi.responses import HTMLResponse
+
+from templating import templates
+
+router = APIRouter()
+
+
+@router.get(
+    "/",
+    name="home",
+)
+def read_docs(request: Request) -> HTMLResponse:
+    context = {}
+    features = [
+        "Create new movie",
+        "Real time statistics",
+    ]
+    context.update(
+        features=features,
+    )
+    return templates.TemplateResponse(
+        request=request,
+        name="home.html",
+        context=context,
+    )
+
+
+@router.get(
+    "/about/",
+    name="about",
+)
+def about_page(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse(
+        request=request,
+        name="about.html",
+    )
