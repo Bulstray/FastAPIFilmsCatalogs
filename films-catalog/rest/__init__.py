c16c916a32b138507bfd1582b5d541819882585b
Ramil Khakimov <bulstray@yandex.ru>
2025-10-11 11:35:59 +0300

    router rest

---
+++ films-catalog/rest/__init__.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+from rest.main_views import router as main_views_router
+from rest.movies import router as list_views_router
+
+router = APIRouter(
+    include_in_schema=False,
+)
+
+router.include_router(main_views_router)
+router.include_router(list_views_router)
