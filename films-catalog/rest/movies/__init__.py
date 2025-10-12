60b7cb6a82636ed1f6e01c2a58531b34125b6885
Ramil Khakimov <bulstray@yandex.ru>
2025-10-12 15:19:41 +0300

    update package rest movies

--- films-catalog/rest/movies/__init__.py
+++ films-catalog/rest/movies/__init__.py
@@ -2,8 +2,12 @@ from fastapi import APIRouter
 
 from rest.movies.list_views import router as list_views_router
 
+from .create_views import router as create_views_router
+
 router = APIRouter(
+    tags=["movies REST"],
     prefix="/movies",
 )
 
 router.include_router(list_views_router)
+router.include_router(create_views_router)
