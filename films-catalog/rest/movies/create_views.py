9bb01d0f7145f117a392c05aeddfe84ad3653986
Ramil Khakimov <bulstray@yandex.ru>
2025-10-13 18:30:21 +0300

    Error handling

--- films-catalog/rest/movies/create_views.py
+++ films-catalog/rest/movies/create_views.py
@@ -1,9 +1,11 @@
 from typing import Annotated, Any
 
-from fastapi import APIRouter, Form, Request
+from fastapi import APIRouter, Form, Request, status
 from fastapi.responses import HTMLResponse, RedirectResponse
 
+from dependencies.movies import GetMoviesStorage
 from schemas.movie import MovieCreate
+from storage.movies.exceptions import MovieAlreadyExists
 from templating import templates
 
 router = APIRouter(
@@ -33,11 +35,43 @@ def get_page_create_movie(request: Request) -> HTMLResponse:
 @router.post(
     "/",
     name="movies:create",
+    response_model=None,
 )
 def create_movie(
+    request: Request,
     movie_create: Annotated[
         MovieCreate,
         Form(),
     ],
-) -> RedirectResponse:
-    return movie_create.model_dump(mode="json")
+    storage: GetMoviesStorage,
+) -> RedirectResponse | HTMLResponse:
+    try:
+        storage.create_or_raise_if_exists(
+            movie_create,
+        )
+    except MovieAlreadyExists:
+        errors = {
+            "slug": f"Movie already with slug {movie_create.slug!r} already exists.",
+        }
+
+    else:
+        return RedirectResponse(
+            url=request.url_for("movies:list"),
+            status_code=status.HTTP_303_SEE_OTHER,
+        )
+
+    context: dict[str, Any] = {}
+    model_schema = MovieCreate.model_json_schema()
+    context.update(
+        model_schema=model_schema,
+        errors=errors,
+        form_validated=True,
+        form_data=movie_create,
+    )
+
+    return templates.TemplateResponse(
+        request=request,
+        name="movies/create.html",
+        context=context,
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+    )
