332095e18c9d0752e7f47bd3ba0698f41206f587
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-10-15 13:25:58 +0300

    refactor blocks in file

--- films-catalog/api/api_v1/films/dependencies.py
+++ films-catalog/api/api_v1/films/dependencies.py
@@ -1,35 +1,19 @@
 import logging
-from typing import Annotated
 
 from fastapi import (
-    Depends,
     HTTPException,
-    Request,
     status,
 )
 from fastapi.security import (
     HTTPAuthorizationCredentials,
-    HTTPBasic,
-    HTTPBasicCredentials,
     HTTPBearer,
 )
 
 from dependencies.movies import GetMoviesStorage
 from schemas.movie import Movie
-
-from ..auth.services.redis_tokens_helper import redis_tokens
-from ..auth.services.redis_users_helper import redis_users
+from services.auth import redis_tokens
 
 log = logging.getLogger(__name__)
-
-UNSAFE_METHODS = frozenset(
-    {
-        "PUT",
-        "PATCH",
-        "DELETE",
-        "POST",
-    },
-)
 
 static_api_token = HTTPBearer(
     auto_error=False,
@@ -52,13 +36,6 @@ def prefetch_film_by_id(
     )
 
 
-user_basic_auth = HTTPBasic(
-    scheme_name="Basic Auth",
-    description="Basic username + password auth",
-    auto_error=False,
-)
-
-
 def validate_api_token(api_token: HTTPAuthorizationCredentials) -> None:
     if redis_tokens.token_exist(
         token=api_token.credentials,
@@ -67,49 +44,4 @@ def validate_api_token(api_token: HTTPAuthorizationCredentials) -> None:
     raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Invalid API token",
-    )
-
-
-def validate_basic_auth(credentials: HTTPBasicCredentials | None) -> None:
-
-    if credentials and redis_users.validate_user_password(
-        username=credentials.username,
-        password=credentials.password,
-    ):
-        return
-
-    raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Invalid username or password",
-        headers={"WWW-Authenticate": "Basic"},
-    )
-
-
-def api_token_or_auth_required_for_unsafe_methods(
-    request: Request,
-    api_token: Annotated[
-        HTTPAuthorizationCredentials | None,
-        Depends(static_api_token),
-    ] = None,
-    credentials: Annotated[
-        HTTPBasicCredentials | None,
-        Depends(user_basic_auth),
-    ] = None,
-) -> None:
-    if request.method not in UNSAFE_METHODS:
-        return
-
-    if credentials:
-        return validate_basic_auth(
-            credentials=credentials,
-        )
-
-    if api_token:
-        return validate_api_token(
-            api_token=api_token,
-        )
-
-    raise HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="API token or basic auth required",
     )
