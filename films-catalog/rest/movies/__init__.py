77bc465d2f2cd9e7c0ba0551c7dafa3e858da2c1
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-10-15 13:28:17 +0300

    add dependencies for router (user_basic_auth_required_for_unsafe_methods)

--- films-catalog/rest/movies/__init__.py
+++ films-catalog/rest/movies/__init__.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from dependencies.auth import user_basic_auth_required_for_unsafe_methods
 from rest.movies.list_views import router as list_views_router
 
 from .create_views import router as create_views_router
@@ -7,6 +8,9 @@ from .create_views import router as create_views_router
 router = APIRouter(
     tags=["movies REST"],
     prefix="/movies",
+    dependencies=[
+        Depends(user_basic_auth_required_for_unsafe_methods),
+    ],
 )
 
 router.include_router(list_views_router)
