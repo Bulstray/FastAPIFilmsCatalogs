0fabdef3facc41175ab700b6b1649bad98608c24
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-10-17 15:21:02 +0300

    update detail_views.py

--- films-catalog/api/api_v1/films/views/detail_views.py
+++ films-catalog/api/api_v1/films/views/detail_views.py
@@ -1,15 +1,10 @@
-from typing import Annotated
+from fastapi import APIRouter, BackgroundTasks, status
 
-from fastapi import APIRouter, BackgroundTasks, Depends, status
-
-from api.api_v1.films.dependencies import prefetch_film_by_id
+from dependencies.movies import MovieBySlug
 from schemas.movie import Movie, MoviePartialUpdate, MovieRead, MovieUpdate
 from storage.movies.crud import storage
 
 router = APIRouter()
-
-
-MovieBySlug = Annotated[Movie, Depends(prefetch_film_by_id)]
 
 
 @router.get(
