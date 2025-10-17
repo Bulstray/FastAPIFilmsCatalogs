64c401f5940df54b47d583f08cdf89333620b165
Bulstray <96692002+Bulstray@users.noreply.github.com>
2025-10-17 15:20:49 +0300

    update dependencies

--- films-catalog/api/api_v1/films/dependencies.py
+++ films-catalog/api/api_v1/films/dependencies.py
@@ -9,8 +9,6 @@ from fastapi.security import (
     HTTPBearer,
 )
 
-from dependencies.movies import GetMoviesStorage
-from schemas.movie import Movie
 from services.auth import redis_tokens
 
 log = logging.getLogger(__name__)
@@ -20,20 +18,6 @@ static_api_token = HTTPBearer(
     scheme_name="Static API token",
     description="Your Static API token from the developer portal",
 )
-
-
-def prefetch_film_by_id(
-    slug: str,
-    storage: GetMoviesStorage,
-) -> Movie:
-    film: Movie | None = storage.get_by_slug(slug=slug)
-    if film:
-        return film
-
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"movie by {slug!r} not found",
-    )
 
 
 def validate_api_token(api_token: HTTPAuthorizationCredentials) -> None:
