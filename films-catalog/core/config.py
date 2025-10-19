dc0e065b5315ec8775b6dbbc408730e9a89fcf32
Ramil Khakimov <bulstray@yandex.ru>
2025-10-19 16:27:57 +0300

    add class SessionConfig

--- films-catalog/core/config.py
+++ films-catalog/core/config.py
@@ -65,6 +65,10 @@ class RedisConfig(BaseModel):
     collections_name: RedisCollectionConfig = RedisCollectionConfig()
 
 
+class SessionConfig(BaseModel):
+    secret_key: str
+
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         case_sensitive=False,
@@ -97,6 +101,7 @@ class Settings(BaseSettings):
 
     logging: LoggingConfig = LoggingConfig()
     redis: RedisConfig = RedisConfig()
+    session: SessionConfig
 
 
 # noinspection PyArgumentList
